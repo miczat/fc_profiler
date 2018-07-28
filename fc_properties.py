6733b397006d5f3a47725f18a8930a14efbab101
miczat <miczat@gmail.com>
2018-07-29 08:25:38 +1000

    doc: added docstrings to all functions

--- fc_properties.py
+++ fc_properties.py
@@ -8,6 +8,7 @@ log = logging.getLogger()
 # -----------------------------------------
 
 def get_fc_name(fc_path):
+    """returns the name of a feature class from the feature class object"""
     base_name = arcpy.Describe(fc_path).baseName
     log.debug("get_crs_name returning: " + base_name)
     return base_name
@@ -18,6 +19,9 @@ def get_fc_name(fc_path):
 # -----------------------------------------
 
 def get_fc_geometry_type(fc_path):
+    """returns the geometry type of a feature class
+       usually in the set {MultiPatch, Multipoint,Point,Polyline,Polygon}
+    """
     geometry_type = arcpy.Describe(fc_path).shapeType
     log.debug("get_fc_geometry_type returning: " + geometry_type)
     return geometry_type
@@ -28,24 +32,33 @@ def get_fc_geometry_type(fc_path):
 # -----------------------------------------
 
 def get_crs_name(fc_path):
+    """returns the coordinate system name of a feature class"""
     sr = arcpy.Describe(fc_path).spatialReference
     log.debug("get_crs_name returning: " + sr.name)
     return sr.name
 
 
 def get_crs_wkid(fc_path):
+    """returns the coordinate system EPSG WKID of a feature class"""
     sr = arcpy.Describe(fc_path).spatialReference
     log.debug("get_crs_wkid returning: " + str(sr.factoryCode))
     return sr.factoryCode
 
 
 def get_crs_type(fc_path):
+    """returns the type of coordiate system of a feature class
+       usually in the set {geographic, projected}
+    """
     sr = arcpy.Describe(fc_path).spatialReference
     log.debug("get_crs_type returning: " + str(sr.type))
     return sr.type
 
 
 def get_crs_units(fc_path):
+    """returns the geometry units of a feature class
+       usually in the set {degrees, meters}
+    """
+
     sr = arcpy.Describe(fc_path).spatialReference
     if sr.type == "Projected":
         units = sr.linearUnitName
