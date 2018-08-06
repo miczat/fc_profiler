2e6cf1bc6670fbb532958a013feaf26690f35e10
miczat <miczat@gmail.com>
2018-08-06 18:49:25 +1000

    feat: Style the XLS for better readbility
    Create a driver (xls_output_driver.py) to call xls_output.py
    Improve the docstring in xls_output.py
    Format heading values as bold

--- xls_output.py
+++ xls_output.py
@@ -8,21 +8,31 @@ log = logging.getLogger()
 
 def write_fc_properties(data, xls_path):
     """"writes the feature class properties to a page in an XLS
+        :param data - a list of key, value pairs as a tuples [ (x,y), (x,y), ...]
+        :param xls_path - the full path of the xls file to write
         pre: the xls file can be crated (user has write permission to folder)
     """
 
     book = xlwt.Workbook()
     sheet = book.add_sheet("fc_properties")
-
     cols = ["A", "B"]
+
+    # write data
+    log.debug("writing " + xls_path)
+
+    heading_style = xlwt.easyxf('font: bold 1')
 
     row_num = 1
     for record in data:
-        sheet.write(row_num, 1, record[0])  # row, column, value
+        sheet.write(row_num, 1, record[0], heading_style)  # row, column, value
         sheet.write(row_num, 2, record[1])  # row, column, value
         row_num = row_num + 1
 
-    log.info("saving " + xls_path)
+
+
+
+    # save
+    log.debug("saving " + xls_path)
     try:
         book.save(xls_path)
     except Error as e:
---
+++ xls_output_driver.py
@@ -0,0 +1,24 @@
+# -------------------------------------------
+# driver for developing xls_output
+# -------------------------------------------
+
+
+from xls_output import write_fc_properties
+
+
+
+data = [("Feature Class", "GDA94_polyline"),
+       ("Parent fGDB", r"C:\tmp\fc_profiler_testdata\fc_profiler_test.gdb"),
+       ("Geometry Type", "Polyline"),
+       ("CRS Name", "GCS_GDA_1994"),
+       ("CRS EPSG WKID", 4283),
+       ("CRS Type", "Geographic"),
+       ("CRS Units", "Degree")]
+
+xls_path=r"C:\tmp\dev.xls"
+
+print("writing...")
+print(write_fc_properties(data, xls_path))
+
+
+
